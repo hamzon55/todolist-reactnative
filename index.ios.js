18fb6c15da97a4646c5f33c3564e475679b242ce
HamZa Jerbi <hamzajerbi@HamZas-Mac.local>
2016-12-06 13:21:27 -0800

    Add List View

--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,107 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+ import React, { Component } from 'react';
+ import {
+   AppRegistry,
+   StyleSheet,
+   Text,
+   ScrollView,
+   Button,
+   View
+ } from 'react-native';
 
-export default class TodoList extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+ export default class TodoList extends Component {
+
+
+
+
+   constructor(props) {
+     super(props)
+     this.clearList = this.clearList.bind(this)
+   }
+   state = {
+     movies: [
+       { id: 1, title: "La Reina del Sur", year: 2011 },
+       { id: 2, title: "Ready Player One", year: 2018 },
+       { id: 3, title: "Samurai Jack", year: 2001 },
+     ]
+   }
+   clearList() {
+     this.setState({ movies: [] })
+   }
+   render() {
+
+     return (
+
+
+
+       <ScrollView style={styles.movieList}>
+        <Text style={styles.header}>TODOAPP</Text>
+
+
+       {this.state.movies.map((movie) => {
+         return (
+           <MovieDetails
+           key={movie.id}
+           movie={movie}
+           />
+         )
+       })}
+       <Button
+       title="Clear list"
+       onPress={this.clearList}
+       />
+       </ScrollView>
+     )
+   }
+ }
+
+
+
+ class MovieDetails extends Component {
+   render() {
+     movie = this.props.movie
+     return (
+       <View style={styles.item}>
+       <Text>{movie.title}</Text>
+
+       </View>
+     )
+   }
+ }
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+
   },
-  welcome: {
-    fontSize: 20,
+  header :{
+    padding :30,
     textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+},
+
+title: {
+  textAlign: 'center',
+  color: 'white',
+},
+
+item: {
+  padding: 15,
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  borderBottomWidth: 1,
+  borderBottomColor: 'whitesmoke',
+},
+
+
+
 });
+
 
 AppRegistry.registerComponent('TodoList', () => TodoList);
